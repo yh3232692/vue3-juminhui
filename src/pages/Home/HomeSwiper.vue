006babb18eae3b24db24a784004ed300c1937c4f
yaoheng <820381536@qq.com>
2019-04-28 17:01:42 +0800

    上传特卖组件
    上传特卖组件，突然意识到自己对Vue的组件化开发有点误区。需要大大的调整一次了。

--- src/pages/Home/HomeSwiper.vue
+++ src/pages/Home/HomeSwiper.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="home-swiper">
-    <swiper :options="swiperOption">
-      <swiper-slide v-for="(slide, index) in sliders" :key="index">
-        <img :src="slide.image" alt>
-      </swiper-slide>
-    </swiper>
-  </div>
+	<div id="home-swiper">
+		<swiper :options="swiperOption">
+		<swiper-slide v-for="(slide, index) in sliders" :key="index">
+			<img :src="slide.image" alt>
+		</swiper-slide>
+		</swiper>
+	</div>
 </template>
 
 <script>
@@ -13,45 +13,45 @@ import "swiper/dist/css/swiper.css";
 import { swiper, swiperSlide } from "vue-awesome-swiper";
 
 export default {
-  name: "home-swiper",
-  props: ["sliders"],
-  data() {
-    return {
-      swiperOption: {
-        //设置swiper组件初始化参数
-        autoplay: {
-          autoplay: true, //自动播放
-          disableOnInteraction: false //控制当前触摸了元素之后也会自动播放
-        },
-        loop: true, //开启loop（无限循环播放）
-        on: {
-          tap: function(e) {
-            alert(this.realIndex); //获取当前点击索引
-          }
-        }
-      }
-    };
-  },
-  mounted() {},
-  components: {
-    swiper,
-    swiperSlide
-  }
+	name: "home-swiper",
+	props: ["sliders"],
+	data() {
+		return {
+			swiperOption: {
+				//设置swiper组件初始化参数
+				autoplay: {
+					autoplay: true, //自动播放
+					disableOnInteraction: false //控制当前触摸了元素之后也会自动播放
+				},
+				loop: true, //开启loop（无限循环播放）
+				on: {
+					tap: function(e) {
+						alert(this.realIndex); //获取当前点击索引
+					}
+				}
+			}
+		};
+  	},
+	mounted() {},
+	components: {
+		swiper,
+		swiperSlide
+	}
 };
 </script>
 
 <style scoped>
 #home-swiper {
-  width: 100%;
+  	width: 100%;
 }
 .swiper-container {
-  height: 2.4rem;
-  background: #cccccc;
+	height: 2.4rem;
+	background: #cccccc;
 }
 img {
-  width: 100%;
-  height: 2.4rem;
-  display: block;
+	width: 100%;
+	height: 2.4rem;
+	display: block;
 }
 </style>
 
